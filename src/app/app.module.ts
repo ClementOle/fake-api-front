68a2930e54a1652da05d0488ae53e0a1a43dc13d
OLEWSKI Clement <clement.olewski@ipilyon.net>
2021-07-05 11:34:11 +0200

    Init component Init header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPostComponent } from './views/add-post/add-post.component';
+import { EditPostComponent } from './views/edit-post/edit-post.component';
+import { SinglePostComponent } from './views/single-post/single-post.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import { ErrorsFormComponent } from './components/errors-form/errors-form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPostComponent,
+    EditPostComponent,
+    SinglePostComponent,
+    PostFormComponent,
+    ErrorsFormComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/errors-form/errors-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ErrorsFormComponent } from './errors-form.component';
+
+describe('ErrorsFormComponent', () => {
+  let component: ErrorsFormComponent;
+  let fixture: ComponentFixture<ErrorsFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ErrorsFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ErrorsFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
