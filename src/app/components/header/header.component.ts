bd26c61bbf93647161b188f38843fb4cbbadcf24
OLEWSKI Clement <clement.olewski@ipilyon.net>
2021-09-02 16:41:45 +0200

    Add logout button to header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthService} from "../../services/auth/auth.service";
+import {Subscription} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  token: string;
+  authSub: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.authSub = this.authService.token.subscribe((token: string) => this.token = token);
   }
 
+  ngOnDestroy() {
+    this.authSub.unsubscribe();
+  }
+
+  onClickLogout() {
+    this.authService.logout()
+      .then(() => this.router.navigateByUrl("/signin"));
+  }
 }
