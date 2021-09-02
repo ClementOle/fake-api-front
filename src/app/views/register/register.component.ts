2dc41cf406008d0f42097031d350d5b5968e799a
OLEWSKI Clement <clement.olewski@ipilyon.net>
2021-09-02 15:35:42 +0200

    Add register component

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {User} from "../../models/user.model";
+import {AuthService} from "../../services/auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  errMsg: string;
+
+  form: FormGroup;
+
+  user: User;
+
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) {
+    this.user = new User('', '', '', '');
   }
 
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.form = this.fb.group({
+      firstname: new FormControl(null, [Validators.required]),
+      lastname: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
+    });
+  }
+
+  onFormSubmitted(): void {
+    if (this.form.valid) {
+      this.authService.register(this.user)
+        .then(() => this.router.navigateByUrl("/"))
+        .catch((errMsg: string) => this.errMsg = errMsg);
+    }
+  }
 }
